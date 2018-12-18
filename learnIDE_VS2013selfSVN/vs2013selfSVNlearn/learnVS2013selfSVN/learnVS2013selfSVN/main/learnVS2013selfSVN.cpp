53161612bd36a80cefacbfdba3ed0f8e06f45ddb
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-18 20:58:23 +0800

    submit from VS2013 2018年12月18日20:58:20

---
+++ learnIDE_VS2013selfSVN/vs2013selfSVNlearn/learnVS2013selfSVN/learnVS2013selfSVN/main/learnVS2013selfSVN.cpp
@@ -0,0 +1,12 @@
+//2018年12月18日18:08:05
+#include <Windows.h>
+
+//int WINAPI WinMain(HWND hwnd, HINSTANCE hPrevInstance,LPWSTR lpCmdLine,int nShowCmd)
+//int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,PSTR szCmdLine, int iCmdShow)
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	iCmdShow = 0;
+	szCmdLine = "这是一个窗口";
+	//system("pause");
+}

65b315ebf02fee8af27584293479a9cb403f3358
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-21 13:51:10 +0800

    暂且编译通过了，但发现问题， 1.playAFile_thirdPartyUse_include.hpp文件中，包含目录路径名不标准， 2.并且路径名错误（类似 目录及其它相关内容写成了thirdPartUse，而不是thirdPartyUse ）；改动这个，将会带来依赖关系图变动，接下来也要更新依赖关系图 接下来要改的问题：问题2 修正前备份2018年12月21日13:50:56

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_playAFile/thirdPartUse_include/directShow_use/playAFile_wantToUseDirectShow_playMovie.hpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_playAFile/thirdPartUse_include/directShow_use/playAFile_wantToUseDirectShow_playMovie.hpp
@@ -1,9 +1,7 @@
 #ifndef __playAFile_wantToUseDirectShow_playMovie_hpp
 #define __playAFile_wantToUseDirectShow_playMovie_hpp
 
-#include "limits.h"
 //#include "../../../../../header/thirdParty_procedureNeedUses/directShowUse/directShowUseProcedure_all_include.hpp"//文件名过长，编译报错
 #include <./header/thirdParty_procedureNeedUses/directShowUse/directShowUseProcedure_all_include.hpp>
-
 
 #endif

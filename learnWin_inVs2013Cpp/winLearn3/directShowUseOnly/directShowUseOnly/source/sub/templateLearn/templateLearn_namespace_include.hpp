af77879e6e01f5ddf90f48a8f83ce4b662820eca
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-22 03:30:16 +0800

    编译通过；；接下来在架构上考虑cppFileX_namespace.h式结构构造头文件；备份2018年12月22日03:30:08

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/sub/templateLearn/templateLearn_namespace_include.hpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/sub/templateLearn/templateLearn_namespace_include.hpp
@@ -13,6 +13,35 @@ namespace aRottenGeneralNameThisX_thisNamespace
 			{
 
 			};
+
+			template <class T1, class T2>
+			class Test
+			{
+			public:
+				Test();
+				Test(T1 d1, T2 d2);
+				template <class U, class V> Test(const Test<U, V> &t);
+				void SetData1(T1 val)
+				{
+					data1 = val;
+				}
+				void SetData2(T2 val)
+				{
+					data2 = val;
+				}
+				T1 GetData1()
+				{
+					return data1;
+				}
+				T2 GetData2()
+				{
+					return data2;
+				}
+			private:
+				T1 data1;
+				T2 data2;
+			};
+
 		}
 	}
 }

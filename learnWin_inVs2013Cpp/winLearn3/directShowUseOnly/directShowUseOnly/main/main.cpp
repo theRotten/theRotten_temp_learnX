d668d57b3c8dfeb596a3b3c8a97341edfc0a07de
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-22 04:00:41 +0800

    编译通过；；；备份2018年12月22日04:00:32

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/main/main.cpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/main/main.cpp
@@ -3,8 +3,6 @@
 int main()
 {
 	
-	::aRottenGeneralNameThisX_thisNamespace::sub::templateLearn::abc::demo();
-
 	::aRottenGeneralNameThisX_thisNamespace::functionSets::persuade_playAFile::playAFile_demo();
 	return 0;
 

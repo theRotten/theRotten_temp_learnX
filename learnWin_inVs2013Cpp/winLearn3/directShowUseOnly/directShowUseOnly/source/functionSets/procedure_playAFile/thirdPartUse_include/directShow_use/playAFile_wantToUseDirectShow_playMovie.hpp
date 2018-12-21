4ff3141f62842434ff9f10f022f54f890b3aca2a
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-21 13:34:14 +0800

    编译通过，并且暂时找了个迁就的办法（在工程里包含工程目录，并且用尖括号包），备份2018年12月21日13:34:12

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_playAFile/thirdPartUse_include/directShow_use/playAFile_wantToUseDirectShow_playMovie.hpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_playAFile/thirdPartUse_include/directShow_use/playAFile_wantToUseDirectShow_playMovie.hpp
@@ -1,6 +1,9 @@
 #ifndef __playAFile_wantToUseDirectShow_playMovie_hpp
 #define __playAFile_wantToUseDirectShow_playMovie_hpp
 
+#include "limits.h"
+//#include "../../../../../header/thirdParty_procedureNeedUses/directShowUse/directShowUseProcedure_all_include.hpp"//文件名过长，编译报错
+#include <./header/thirdParty_procedureNeedUses/directShowUse/directShowUseProcedure_all_include.hpp>
 
 
 #endif

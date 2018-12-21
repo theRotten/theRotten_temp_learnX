45e4c86df431c8f33d69c5eeb050f4e80f8337a2
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-21 15:49:22 +0800

    编译通过；；；备份2018年12月21日15:49:20

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/header/thirdParty_procedureNeedUses/directShowUse/directShowUseProcedure_all_include.hpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/header/thirdParty_procedureNeedUses/directShowUse/directShowUseProcedure_all_include.hpp
@@ -1,6 +1,6 @@
 #ifndef __directShowUseProcedure_all_include_hpp
 #define __directShowUseProcedure_all_include_hpp
 
-
+#include "./useCOM/initialCOM.h"
 
 #endif
---
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/header/thirdParty_procedureNeedUses/directShowUse/useCOM/initialCOM.h
@@ -0,0 +1,7 @@
+#ifndef __initialCOM_hpp
+#define __initialCOM_hpp
+
+//说明源自：https://docs.microsoft.com/zh-cn/windows/desktop/directshow/how-to-play-a-file
+#include <.\header\thirdParty_useNeeds\useCOM\useCOM_needsInclude.hpp>
+
+#endif

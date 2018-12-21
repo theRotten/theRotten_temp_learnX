5502f6ed526e013aabbd86a52621671ab9a91833
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-22 00:33:31 +0800

    编译成功；；；备份2018年12月22日00:33:28

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_playAFile/playAFile.cpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_playAFile/playAFile.cpp
@@ -13,7 +13,7 @@ void ::____aRottenGeneralNameThisX_thisNamespace_thisUse_randomMacro_20181220204
 	//2.接下来，调用CoCreateInstance以创建Filter Graph Manager：
 	IGraphBuilder *pGraph;
 	hr = CoCreateInstance(CLSID_FilterGraph, NULL,
-		CLSCTX_INPROC_SERVER, IID_IGraphBuilder, (void **)&pGraph);
+		CLSCTX_INPROC_SERVER, IID_IGraphBuilder, (void **)&pGraph);//这条执行之后，出现了1个新线程：quartz.dll!ObjectThread
 
 	//3.Filter Graph Manager公开了这两个接口。使用返回的IGraphBuilder指针来查询它们：
 	IMediaControl *pControl;
@@ -22,10 +22,10 @@ void ::____aRottenGeneralNameThisX_thisNamespace_thisUse_randomMacro_20181220204
 	hr = pGraph->QueryInterface(IID_IMediaEvent, (void **)&pEvent);
 
 	//4.现在您可以构建过滤器图形了。对于文件回放，这是通过单个方法调用完成的：
-	hr = pGraph->RenderFile(L"G:\\files\\using\\classified\\githubLocalDir\\theRotten\\theRotten_temp_learnX\\learnWin_inVs2013Cpp\\winLearn3\\directShowUseOnly\\directShowUseOnly\\test\\testFile\\functionSets_procedure_playAFile_test_testAviFile_input.avi", NULL);
-
+	hr = pGraph->RenderFile(L"G:\\files\\using\\classified\\githubLocalDir\\theRotten\\theRotten_temp_learnX\\learnWin_inVs2013Cpp\\winLearn3\\directShowUseOnly\\directShowUseOnly\\test\\testFile\\functionSets_procedure_playAFile_test_testAviFile_input.avi", NULL);//这条执行完之后，出现了2个新线程：ntdll.dll!_TppWaiterpThread@4	quartz.dll!CAMThread::InitialThreadProc()
+	
 	//5.如果指定的文件不存在，或者无法识别文件格式，则此方法可能会失败。但是，假设该方法成功，则现在可以使用过滤器图表进行回放。要运行图形，请调用IMediaControl :: Run方法：
-	hr = pControl->Run();
+	hr = pControl->Run();//这条启动执行后，新窗口开始播放，并多了6个进程，其中3个是ntdll.dll!_TppWorkerThread@4，1个是quartz.dll!CBaseReferenceClock::AdviseThreadFunction()，1个是quartz.dll!CAMThread::InitialThreadProc() ， 1个是 quartz.dll!CAsyncFile::InitialThreadProc()
 
 	//6.当过滤器图形运行时，数据将在过滤器中移动，并呈现为视频和音频。播放发生在单独的线程上。您可以通过调用IMediaEvent :: WaitForCompletion方法等待播放完成：
 	long evCode = 0;

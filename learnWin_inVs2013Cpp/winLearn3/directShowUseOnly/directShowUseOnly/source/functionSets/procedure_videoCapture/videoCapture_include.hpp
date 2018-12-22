8c9e79ac6573930db88634dea0e039d04a78a94f
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-23 00:04:20 +0800

    编译通过；；筛选器内容管理方式v0.2；备份2018年12月22日23:57:06

---
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_videoCapture/videoCapture_include.hpp
@@ -0,0 +1,10 @@
+#ifndef __videoCapture_include_hpp
+#define __videoCapture_include_hpp
+//该成分在该架构成分内当前结构版本 v0.3.1.2
+//该成分在该结构成分所属架构位置描述：该独立[“功能集set成分”]构件的（实际实质）内容,要包含的内容。
+
+#include "./videoCapture_namespace.hpp"
+
+#include "./videoCapture_thirdPartyUse_include.hpp"
+
+#endif
---
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_videoCapture/videoCapture_namespace.hpp
@@ -0,0 +1,15 @@
+#ifndef __videoCapture_namespace_hpp
+#define __videoCapture_namespace_hpp
+//该成分在该架构成分内当前结构版本 v0.3.1.2
+//该成分在该结构成分所属架构位置描述：该独立[“功能集set成分”]构件的（实际实质）内容的命名空间。
+
+#include "./videoCapture_namespace_include.hpp"
+
+//！@注意：改这个头文件命名空间声明结构的时候，一定要下面这一行宏定义的值，这个宏定义本身不能和其它宏定义重复，这个宏定义的值要和实际命名空间匹配
+#define ____aRottenGeneralNameThisX_thisNamespace_thisUse_randomMacro_201812222335_fadiobnopaenfbpabn aRottenGeneralNameThisX_thisNamespace
+namespace aRottenGeneralNameThisX_thisNamespace
+{
+
+}
+
+#endif

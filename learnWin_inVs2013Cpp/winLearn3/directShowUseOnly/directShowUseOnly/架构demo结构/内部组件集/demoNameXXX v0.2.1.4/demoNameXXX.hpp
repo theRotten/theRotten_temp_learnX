22c4d6d6b05f659b503df9999cb93b94649ae134
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-22 23:19:09 +0800

    编译通过；；；备份2018年12月22日23:19:07

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/架构demo结构/内部组件集/demoNameXXX v0.2.1.4/demoNameXXX.hpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/架构demo结构/内部组件集/demoNameXXX v0.2.1.4/demoNameXXX.hpp
@@ -1,7 +1,7 @@
 #ifndef __demoNameXXX_hpp
 #define __demoNameXXX_hpp
 //该成分在该架构成分内当前结构版本 v0.2.1.4
-//该成分在该结构成分所属架构位置描述：用于外部调用该独立[“功能集set成分”]构件时，要包含的内容。
+//该成分在该结构成分所属架构位置描述：用于外部调用该独立[“内部组件集set成分”]构件时，要包含的内容。
 
 #include "./demoNameXXX_namespace.hpp"
 
--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/架构demo结构/内部组件集/demoNameXXX v0.2.1.4/demoNameXXX_namespace_include.hpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/架构demo结构/内部组件集/demoNameXXX v0.2.1.4/demoNameXXX_namespace_include.hpp
@@ -1,7 +1,7 @@
 #ifndef __demoNameXXX_namespace_include_hpp
 #define __demoNameXXX_namespace_include_hpp
 //该成分在该架构成分内当前结构版本 v0.2.1.4
-//该成分在该结构成分所属架构位置描述：该独立[“功能集set成分”]构件的（实际实质）内容的命名空间所需要包含的内容。
+//该成分在该结构成分所属架构位置描述：该独立[“内部组件集set成分”]构件的（实际实质）内容的命名空间所需要包含的内容。
 
 
 

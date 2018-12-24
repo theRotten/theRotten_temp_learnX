5620a864a0b8e43455b5a9b22a7a77a9666c90ba
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-24 15:42:20 +0800

    编译通过；；；备份2018年12月24日15:42:13

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/main/main.cpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/main/main.cpp
@@ -1,6 +1,20 @@
 #include "./main_include.hpp"
 
 
+void mainTempX(void)
+{
+	using namespace std;
+	cout << "gpk多字节" << endl;
+	wcout << "w多字节编码" << endl;
+}
+
+void mainTemp(void)
+{
+	using namespace std;
+	cout << "gpk多字节" << endl;
+	wcout << "w多字节编码" << endl;
+}
+
 int mainTest(void)
 {
 	return 0;
@@ -8,17 +22,47 @@ int mainTest(void)
 
 void mainTestX(void)
 {
-	using namespace std;
-	cout << "gpk多字节" << endl;
-	wcout << "w多字节编码" << endl;
+	//测试videoCapture模块videoCapture_main();
+	::aRottenGeneralNameThisX_thisNamespace::functionSets::procedure_videoCapture::videoCapture_main();
+
 }
+
+void mainStopHere(void)
+{
+	std::cout << std::endl << "mainStopHere" << std::endl;
+	std::system("pause");
+}
+
+void mainStopWith(void)
+{
+	std::cout << std::endl << "mainStopWith" << std::endl;
+	system("pause");
+}
+
+void mainPauseWith(void)
+{
+	std::cout << std::endl << "mainPauseWith" << std::endl;
+	system("pause");
+}
+
+void mainStopWait(void)
+{
+	std::cout << std::endl << "mainStopWait" << std::endl;
+	system("pause");
+}
+
+void mainPauseWait(void)
+{
+	std::cout << std::endl << "mainPauseWait" << std::endl;
+	system("pause");
+}
+
 
 int main()
 {
 	mainTestX();
-	::aRottenGeneralNameThisX_thisNamespace::functionSets::procedure_videoCapture::videoCapture_main();
 	
-	system("pause");
+	mainStopWait();
 	return 0;
 
 }

b985658e1588afa55e6109be7404eb48a73c0ee1
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-24 13:23:12 +0800

    编译通过；；；备份2018年12月24日13:23:10

---
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/header/thirdParty_useNeeds/useXX/useXX_needsInclude.hpp
@@ -0,0 +1,9 @@
+#ifndef __useXX_needsInclude_hpp
+#define __useXX_needsInclude_hpp
+
+//说明源自：https://这是一个列举的demo例子。。。。假设这是要调用一个叫作XX的独立第三方组件
+#include<iostream>
+
+#pragma comment(lib,"./lib/demoLib/LIBC.lib")
+
+#endif

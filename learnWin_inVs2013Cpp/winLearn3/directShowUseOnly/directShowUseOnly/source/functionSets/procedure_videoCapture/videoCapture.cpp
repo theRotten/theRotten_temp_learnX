ffb01bce97a826dd9959a7c4911e82bcf23393db
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-25 14:50:50 +0800

    编译通过；；；备份2018年12月25日14:50:28

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_videoCapture/videoCapture.cpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/source/functionSets/procedure_videoCapture/videoCapture.cpp
@@ -22,8 +22,15 @@ void ::____aRottenGeneralNameThisX_thisNamespace_thisUse_randomMacro_20181224135
 	//测试试验 目的：去实现一个让摄像头在屏幕上预览
 
 	IMoniker *pMoniker;//用于接收摄像头设备的昵称
+	
+	//创建GraphBuilder类指针 GraphBuilder周期_创建指针 开始----------------------------------2018年12月25日14:10:05
 	IGraphBuilder **m_pGraph=NULL;//用于接收FilterGraph
+	//创建GraphBuilder类指针 GraphBuilder周期_创建指针 结束----------------------------------2018年12月25日14:10:05
+
+	//创建设备枚举类指针 设备枚举周期_创建指针 开始----------------------------------2018年12月25日11:25 : 26
 	IEnumMoniker *pEnum;//用于接收枚举
+	//创建设备枚举类指针 设备枚举周期_创建指针 结束----------------------------------2018年12月25日11:25 : 26
+
 	IBaseFilter *pCap = NULL;
 
 	HRESULT hr;//用于接收返回结果
@@ -32,6 +39,7 @@ void ::____aRottenGeneralNameThisX_thisNamespace_thisUse_randomMacro_20181224135
 	hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
 	//COM组件初始化流程 代码段 结束--------------------------------------------------------2018年12月25日09:52:11
 
+	//创建GraphBuilder类实例 GraphBuilder周期_实例化 开始----------------------------------2018年12月25日14:12:03
 	//创建一个Graph 代码段 开始--------------------------------------------------------2018年12月24日21:49:42
 	//2.接下来，调用CoCreateInstance以创建Filter Graph Manager：
 	//IGraphBuilder *pGraph;
@@ -39,10 +47,15 @@ void ::____aRottenGeneralNameThisX_thisNamespace_thisUse_randomMacro_20181224135
 		CLSCTX_INPROC_SERVER, IID_IGraphBuilder, (void **)m_pGraph);//创建CLSID_FilterGraph种类的实例，非聚合COM对象，线程内运行上下文，通讯接口（和IID_IGraphBuilder进行），用m_pGraph接收创建出来的实例//这条执行之后，出现了1个新线程：quartz.dll!ObjectThread
 	//如图所示，类标识符（CLSID）是CLSID_FilterGraph。Filter Graph Manager由进程内DLL提供，因此执行上下文为CLSCTX_INPROC_SERVER。DirectShow支持自由线程模型，因此您也可以使用COINIT_MULTITHREADED标志调用CoInitializeEx。
 	//创建一个Graph 代码段 结束--------------------------------------------------------2018年12月24日21:49:42
+	//创建GraphBuilder类实例 GraphBuilder周期_实例化 结束----------------------------------2018年12月25日14:12:03
 
 	//对摄像头类型设备进行枚举 代码段内部段 开始----------------------------------2018年12月25日09:59:56
 	hr = EnumerateDevices(CLSID_VideoInputDeviceCategory, &pEnum);//CLSID_VideoInputDeviceCategory是摄像头（视频输入设备）类的GUID
 	//对摄像头类型设备进行枚举 代码段内部段 结束----------------------------------2018年12月25日09:59:56
+
+	//显示枚举出设备的信息 代码段 开始----------------------------------2018年12月25日14:04:54
+	DisplayDeviceInformation(pEnum);
+	//显示枚举出设备的信息 代码段 结束----------------------------------2018年12月25日14:04:54
 
 	//用于释放‘设备的枚举’ 设备枚举周期_释放 开始----------------------------------2018年12月25日11:25:26
 	pEnum->Release();
@@ -55,8 +68,9 @@ void ::____aRottenGeneralNameThisX_thisNamespace_thisUse_randomMacro_20181224135
 	
 
 
-	getCaptureMoniker(pMoniker);
-	createADirectShowCaptureFilterForTheDevice(pMoniker,*m_pGraph);//添加摄像头的captureFilter到graph
+	//getCaptureMoniker(pMoniker);
+	//createADirectShowCaptureFilterForTheDevice(pMoniker,*m_pGraph);//添加摄像头的captureFilter到graph
+
 	//previewTheCapture(m_pGraph, IBaseFilter* &pCap);//在某个【ppGraph】上preview 【pCap】
 	
 

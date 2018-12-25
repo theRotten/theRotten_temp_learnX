c22fea4232051ac59471b0a280b6da99ac6080e7
theRotten <45709306+theRotten@users.noreply.github.com>
2018-12-25 13:53:00 +0800

    编译通过；；；备份2018年12月25日13:52:42

--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/架构Demo结构/source/内部组件集/demoNameXXX v0.2.1.4/demoNameXXX.hpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/架构Demo结构/source/内部组件集/demoNameXXX v0.2.1.4/demoNameXXX.hpp
@@ -4,5 +4,6 @@
 //该成分在该结构成分所属架构位置描述：用于外部调用该独立[“内部组件集set成分”]构件时，要包含的内容。
 
 #include "./demoNameXXX_namespace.hpp"
+#include "./demoNameXXX_helper_namespace.hpp"
 
 #endif
--- learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/架构Demo结构/source/内部组件集/demoNameXXX v0.2.1.4/demoNameXXX_helper_namespace.hpp
+++ learnWin_inVs2013Cpp/winLearn3/directShowUseOnly/directShowUseOnly/架构Demo结构/source/内部组件集/demoNameXXX v0.2.1.4/demoNameXXX_helper_namespace.hpp
@@ -3,7 +3,7 @@
 //该成分在该架构成分内当前结构版本 v0.2.1.4
 //该成分在该结构成分所属架构位置描述：该独立[“内部组件集set成分”]构件的（实际实质）内容的可对外提供的助手（协助助理助力）内容成分命名空间。
 
-#include "./demoNameXXX_helper_namespace_include.hpp"
+#include "./demoNameXXX_hp_namespace_inc.hpp"
 
 //！@注意：改这个头文件命名空间声明结构的时候，一定要下面这一行宏定义的值，这个宏定义本身不能和其它宏定义重复，这个宏定义的值要和实际命名空间匹配
 #define ____aRottenGeneralNameThisX_thisNamespace_thisUse_randomMacro_201812251221_helperabcdefgSuibianxiedeADemoNameSpaceAbcdefgLuanqibazao aRottenGeneralNameThisX_thisNamespace::sub
